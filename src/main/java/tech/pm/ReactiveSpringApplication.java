b82b0ec2d35428497618e782cef09b3aecff56bc
sergey-judi <serega7qwerty@gmail.com>
2022-05-17 21:07:46 +0300

    Bootstrapped project

---
+++ src/main/java/tech/pm/ReactiveSpringApplication.java
@@ -0,0 +1,13 @@
+package tech.pm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactiveSpringApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(ReactiveSpringApplication.class, args);
+  }
+
+}
