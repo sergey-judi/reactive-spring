6a0f57c8f5756cc6f2541a49c4000963876cebb6
sergey-judi <serega7qwerty@gmail.com>
2022-05-17 21:15:56 +0300

    Added Quote model

---
+++ src/main/java/tech/pm/model/Quote.java
@@ -0,0 +1,18 @@
+package tech.pm.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Data
+@Document
+@NoArgsConstructor
+@AllArgsConstructor
+public class Quote {
+
+  private String id;
+  private String book;
+  private String content;
+
+}
